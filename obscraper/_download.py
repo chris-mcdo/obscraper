d07e86e2e5973b8083ce64e75709cb0497682544
Christopher McDonald <cmcdonal33@gmail.com>
2022-02-08 22:12:01 +0000

    improved async download

--- obscraper/_download.py
+++ obscraper/_download.py
@@ -3,45 +3,50 @@
 This interface is internal - implementation details may change.
 """
 import functools
-import random
-import asyncio
+import trio
 
+from . import _exceptions
 from ._extract_post import name_to_url
 
-START_DELAY = 0.04
-INCREASE_FACTOR = 4
-MAX_DELAY = 3
-MAX_REQUESTS = 5
+INCREASE_FACTOR = 2
+MAX_DELAY = 5
+MAX_REQUESTS = 10
 
 VOTE_API_URL = ('https://www.overcomingbias.com/'
                 'wp-content/plugins/gd-star-rating/ajax.php')
 COMMENT_API_URL = 'https://overcoming-bias.disqus.com/count-data.js'
 EDIT_DATES_URL = 'https://www.overcomingbias.com/post.xml'
 
+# Name used to update the vote auth code
+VOTE_AUTH_UPDATE_NAME = '/2011/12/life-is-good'
 
-def async_retry_request(func):
+
+def async_retry_rate_limited(rate_limit):
     """Retry HTTP request until 429 response is no longer received."""
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        delay = START_DELAY
-        for _ in range(MAX_REQUESTS):
-            response = await func(*args, **kwargs)
-            if response.status_code != 429:
-                break
-            try:
-                delay = int(response.headers['Retry-After'])
-            except (KeyError, TypeError):
-                delay = delay * (1 + random.random()) / 2
-            if delay > MAX_DELAY:
-                break
-            await asyncio.sleep(delay)
-            delay = delay * INCREASE_FACTOR
-        return response
-    return wrapper
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            delay = rate_limit
+            for _ in range(MAX_REQUESTS):
+                # Make sure this request is at least delay from prev one
+                response = await func(*args, **kwargs)
+                if response.status_code != 429:
+                    break
+                try:
+                    delay = int(response.headers['Retry-After'])
+                except (KeyError, TypeError):
+                    delay = delay * INCREASE_FACTOR
+                if delay > MAX_DELAY:
+                    raise _exceptions.InvalidResponseError('Exceeded max'
+                                                           ' timeout.')
+                await trio.sleep(delay)
+            return response
+        return wrapper
+    return decorator
 
 
-@async_retry_request
-async def download_post(name, async_client):
+@async_retry_rate_limited(rate_limit=0.04)
+async def download_post(async_client, name):
     """Download a post by its name."""
     headers = get_default_headers()
     url = name_to_url(name)
@@ -49,8 +54,8 @@ async def download_post(name, async_client):
     return response
 
 
-@async_retry_request
-async def download_vote_count(vote_id, vote_auth, async_client):
+@async_retry_rate_limited(rate_limit=0.04)
+async def download_vote_count(async_client, vote_id, vote_auth):
     """Download vote count for a post."""
     headers = get_default_headers()
     headers.update({'x-requested-with': 'XMLHttpRequest'})
@@ -58,15 +63,15 @@ async def download_vote_count(vote_id, vote_auth, async_client):
         '_ajax_nonce': vote_auth,
         'vote_type': 'cache',
         'vote_domain': 'a',
-        'votes': vote_id
+        'votes': f'atr.{vote_id}'
     }
     response = await async_client.post(VOTE_API_URL, headers=headers,
                                        params=params)
     return response
 
 
-@async_retry_request
-async def download_comment_count(comment_id, async_client):
+@async_retry_rate_limited(rate_limit=0.001)
+async def download_comment_count(async_client, comment_id):
     """Download comment count for a post."""
     headers = get_default_headers()
     params = {'1': comment_id}
@@ -75,7 +80,7 @@ async def download_comment_count(comment_id, async_client):
     return response
 
 
-@async_retry_request
+@async_retry_rate_limited(rate_limit=0.001)
 async def download_edit_dates(async_client):
     """Download list of posts and edit dates."""
     headers = get_default_headers()
